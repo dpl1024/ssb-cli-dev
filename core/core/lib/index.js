691a880275fbbbce4746f341bc8b55a24fad3357
imroc <dpl1024@163.com>
2022-09-14 00:31:21 +0800

    NodeJS版本检查 semver npm版本比较

--- core/core/lib/index.js
+++ core/core/lib/index.js
@@ -9,10 +9,26 @@ module.exports = core;
  */
 
 const log = require('@ssb-cli-dev/log');
+const semver = require('semver');
+const colors = require('colors/safe');
 const pkg = require('../package.json');
+const constant = require('./const');
 
 function core() {
-  checkPkgVersion();
+  try {
+    checkPkgVersion();
+    checkNodeVersion();
+  } catch (e) {
+    log.error(e.message);
+  }
+}
+
+function checkNodeVersion() {
+  const currentVersion = process.version;
+  const lowestVersion = constant.LOWEST_NODE_VERSION;
+  if (!semver.gte(currentVersion, lowestVersion)) {
+    throw new Error(colors.red(`ssb-cli 需要安装 ${lowestVersion} 以上版本的 NodeJS`));
+  }
 }
 
 function checkPkgVersion() {
